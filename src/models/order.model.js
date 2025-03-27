a7592c85ab5db2b05a6026351d2576ed61b85e6b
Abdurahmon <abdurahmonbekmirzayev3@gmail.com>
2025-03-27 12:47:42 +0500

    Orders CRUD va model tugatildi

---
+++ src/models/order.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    total_price: {
+      type: Number,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    orderItems: [
+      {
+        clothesId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Clothes",
+          required: true,
+        },
+        count: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+export default Order;
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./user.route.js";
+import orderRouter from "./order.route.js";
 
 const route = Router();
 
 route.use("/users", userRouter);
+route.use("/orders", orderRouter);
 
 export default route;
