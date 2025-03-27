08c4b0841bfb969373f027d971c6bd2a79964742
Shahlo <nematovashaxloxon@gmail.com>
2025-03-27 10:44:43 +0500

    schema folder added

---
+++ src/middleware/validation.middleware.js
@@ -0,0 +1,14 @@
+export const VolidationMiddleware = (schema) => {
+    return (req,res,next) => {
+        const { error,value } = schema.validate(req.body);
+
+        if(error){
+            return res.status(400).send({
+                message: error.message
+            })
+        }
+        req.body = value;
+
+        next()
+    };
+};
