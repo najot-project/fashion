9fc7d9485bcc88e5610abee6c12ab03dca1e0d0d
asal.kh <asalkhamidova@MacBook-Pro-asal.local>
2025-03-28 09:59:36 +0500

    Local changes before updating develop

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import UserController from "../controllers/user.controller.js";
-import validationMiddleware from "../middlewares/validation.middleware.js";
+import { VolidationMiddleware } from "../middleware/validation.middleware.js";
 import { userSchema } from "../schema/user.schema.js";
 
 const userRouter = Router();
@@ -8,8 +8,8 @@ const userRouter = Router();
 userRouter
 .get("/",UserController.getAllUsers)
 .get("/:id",UserController.getOneUser)
-.post("/",validationMiddleware(userSchema),UserController.createUser)
-.put("/:id", validationMiddleware(userSchema), UserController.updateUser)
+.post("/",VolidationMiddleware(userSchema),UserController.createUser)
+.put("/:id", VolidationMiddleware(userSchema), UserController.updateUser)
 .delete("/:id", UserController.deleteUser)
 
 export default userRouter;
