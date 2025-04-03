80e6f84ba216f38bf4032d0f5ed084b5ba51b8e1
Abdurahmon <abdurahmonbekmirzayev3@gmail.com>
2025-04-03 15:01:27 +0500

    Order full finished

--- src/routers/order.route.js
+++ src/routers/order.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import orderController from "../controllers/order.controller.js";
+import { ValidationMiddleware } from "../middleware/validation.middleware.js";
+import { createOrderSchema } from "../schema/order.schema.js";
 
 const orderRouter = Router();
 
 orderRouter
     .get("/", orderController.getOrders)
     .get("/:id", orderController.getOrderById)
-    .post("/", orderController.createOrder)
+    .post("/",
+        ValidationMiddleware(createOrderSchema),
+        orderController.createOrder)
     .put("/:id", orderController.updateOrder)
     .delete("/:id", orderController.deleteOrder);
 
