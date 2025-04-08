c35f4362404c2c488291c713c5da0e229e2686e6
asal.kh <asalkhamidova@MacBook-Pro-asal.local>
2025-04-08 14:30:04 +0500

    ejs fixed for orders

--- src/routers/page.route.js
+++ src/routers/page.route.js
@@ -48,4 +48,21 @@ pageRouter.get("/orders", async (req, res, next) => {
     }
   });
 
+
+pageRouter.get("/orders", async (req, res, next) => {
+      res.render("orders/index", { orders });
+})
+  
+
+  pageRouter.get("/orders/create", async (req, res, next) => { 
+      res.render("orders/create", { clothes });
+})
+  
+  pageRouter.get("/orders/:id", async (req, res, next) => {
+        const { id } = req.params;
+        const order = await orderModel.findById(id);
+        res.render("orders/show", { order });
+    }
+   )
+
 export default pageRouter;
